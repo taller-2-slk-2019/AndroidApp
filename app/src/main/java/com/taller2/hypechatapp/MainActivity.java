be9ca0f32b78e660f4feac469f5c7a35c846cc0f
aguszorza <aguszorza@gmail.com>
2019-03-18 00:45:43 -0300

    Agregado de Log al cambiar de estado la aplicacion

--- app/src/main/java/com/taller2/hypechatapp/MainActivity.java
+++ app/src/main/java/com/taller2/hypechatapp/MainActivity.java
@@ -2,12 +2,45 @@ package com.taller2.hypechatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    String msg = "Android : ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
     }
 }
