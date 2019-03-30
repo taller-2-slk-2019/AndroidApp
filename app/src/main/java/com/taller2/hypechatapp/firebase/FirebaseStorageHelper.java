9b3a83ed1a868092a45e8887e8d5e10c46322df8
matias feld <matias.feld@mdevz.com>
2019-03-30 16:00:43 -0300

    firebase file upload working

--- app/src/main/java/com/taller2/hypechatapp/firebase/FirebaseStorageHelper.java
+++ app/src/main/java/com/taller2/hypechatapp/firebase/FirebaseStorageHelper.java
@@ -3,13 +3,15 @@ package com.taller2.hypechatapp.firebase;
 import android.net.Uri;
 import android.util.Log;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.File;
+import java.util.UUID;
 
 import androidx.annotation.NonNull;
 
@@ -21,25 +23,34 @@ public class FirebaseStorageHelper {
         storageRef = FirebaseStorage.getInstance().getReference();
     }
 
-    public void uploadLocalFile(FirebaseStorageInterface caller, String filePath){
-        Uri file = Uri.fromFile(new File(filePath));
-        StorageReference filesRef = storageRef.child("files");
-        this.upload(caller, filesRef, file);
+    public void uploadLocalFile(FirebaseStorageInterface caller, Uri filePath){
+        this.upload(caller, "files/", filePath);
     }
 
-    public void uploadLocalImage(final FirebaseStorageInterface caller, String imagePath){
-        Uri file = Uri.fromFile(new File(imagePath));
-        StorageReference imagesRef = storageRef.child("images");
-        this.upload(caller, imagesRef, file);
+    public void uploadLocalImage(final FirebaseStorageInterface caller, Uri imagePath){
+        this.upload(caller, "images/", imagePath);
     }
 
-    private void upload(final FirebaseStorageInterface caller, StorageReference storage, Uri file){
-        storage.putFile(file)
-            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+    private void upload(final FirebaseStorageInterface caller, String folder, Uri file){
+        final StorageReference storage = storageRef.child(folder + UUID.randomUUID().toString() + file.getLastPathSegment());
+        UploadTask uploadTask = storage.putFile(file);
+        uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+
+                // Continue with the task to get the download URL
+                return storage.getDownloadUrl();
+            }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
                 @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                public void onComplete(@NonNull Task<Uri> task) {
                     // Get a URL to the uploaded content
-                    Uri downloadUrl = taskSnapshot.getStorage().getDownloadUrl().getResult();
+                    Uri downloadUrl = task.getResult();
+                    Log.i("Firebase file uploaded", downloadUrl.toString());
+
                     if (caller != null){
                         caller.onFileUploaded(downloadUrl.toString());
                     }
