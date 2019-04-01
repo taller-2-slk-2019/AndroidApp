2c280fe7edce583011983b729db6e8dfa9313a02
matias feld <matias.feld@mdevz.com>
2019-04-01 17:25:13 -0300

    removed image picker code from organization activity

---
+++ app/src/main/java/com/taller2/hypechatapp/components/ImagePicker.java
@@ -0,0 +1,48 @@
+package com.taller2.hypechatapp.components;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.button.MaterialButton;
+import com.taller2.hypechatapp.R;
+
+import java.io.IOException;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImagePicker {
+
+    public static final int PICK_IMAGE_REQUEST = 71;
+
+    public Intent getImagePickerIntent(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        return Intent.createChooser(intent, "Seleccionar Imagen");
+    }
+
+
+    public Uri analyzeResult(AppCompatActivity activity, Intent data) {
+        Uri filePath = data.getData();
+        try {
+            Bitmap profileImageBitmap = MediaStore.Images.Media.getBitmap(activity.getContentResolver(), filePath);
+
+            ImageView profileImageView = activity.findViewById(R.id.profile_image_view);
+            profileImageView.setImageBitmap(profileImageBitmap);
+            profileImageView.setVisibility(View.VISIBLE);
+
+            MaterialButton pickImageButton = activity.findViewById(R.id.pick_profile_image_button);
+            pickImageButton.setVisibility(View.INVISIBLE);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return filePath;
+    }
+}
--- app/src/main/java/com/taller2/hypechatapp/ui/activities/CreateOrganizationActivityStepOne.java
+++ app/src/main/java/com/taller2/hypechatapp/ui/activities/CreateOrganizationActivityStepOne.java
@@ -1,10 +1,8 @@
 package com.taller2.hypechatapp.ui.activities;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,9 +10,8 @@ import android.widget.ImageView;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.taller2.hypechatapp.R;
+import com.taller2.hypechatapp.components.ImagePicker;
 import com.taller2.hypechatapp.network.model.OrganizationRequest;
-
-import java.io.IOException;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -23,15 +20,14 @@ import static android.content.Intent.FLAG_ACTIVITY_FORWARD_RESULT;
 
 public class CreateOrganizationActivityStepOne extends AppCompatActivity {
 
-    private static final int REQUEST_CODE = 2;
-    private final int PICK_IMAGE_REQUEST = 71;
     private Uri filePath;
-    private Bitmap profileImageBitmap;
+    private ImagePicker imagePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_organization_step1);
+        imagePicker = new ImagePicker();
 
         setUpUI();
     }
@@ -102,32 +98,16 @@ public class CreateOrganizationActivityStepOne extends AppCompatActivity {
     }
 
     private void chooseImage() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+        startActivityForResult(imagePicker.getImagePickerIntent(), imagePicker.PICK_IMAGE_REQUEST);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-                && data != null && data.getData() != null )
-        {
-            filePath = data.getData();
-            try {
-                profileImageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                ImageView profileImageView=findViewById(R.id.profile_image_view);
-                profileImageView.setImageBitmap(profileImageBitmap);
-                profileImageView.setVisibility(View.VISIBLE);
-                MaterialButton pickImageButton=findViewById(R.id.pick_profile_image_button);
-                pickImageButton.setVisibility(View.INVISIBLE);
 
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
+        if(requestCode == imagePicker.PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null ) {
+            filePath = imagePicker.analyzeResult(this, data);
         }
     }
 }
