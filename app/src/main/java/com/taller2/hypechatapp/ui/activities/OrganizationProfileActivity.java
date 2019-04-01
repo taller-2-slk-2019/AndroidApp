dc18aa8eb56e1d600a203b40b3538df805b3fadc
aguszorza <aguszorza@gmail.com>
2019-04-01 17:13:50 -0300

    Inicio el style de los perfiles

--- app/src/main/java/com/taller2/hypechatapp/ui/activities/OrganizationProfileActivity.java
+++ app/src/main/java/com/taller2/hypechatapp/ui/activities/OrganizationProfileActivity.java
@@ -49,7 +49,7 @@ public class OrganizationProfileActivity extends AppCompatActivity {
             @Override
             public void onResponseSuccess(Organization responseBody) {
                 name.setText(responseBody.getName());
-                description.setText(responseBody.getDescription());
+                description.setText(responseBody.getDescription() + "\nEsta\nDescripcion\nes\nlarga\ntal\nvez");
                 welcomeMessage.setText(responseBody.getWelcome());
                 dialog.dismiss();
             }
