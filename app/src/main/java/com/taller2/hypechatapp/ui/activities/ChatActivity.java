1136f10ece9278a95a35a2172f796984ec730723
matias feld <matias.feld@mdevz.com>
2019-04-11 20:52:14 -0300

    refactor menu and channel chat activity

---
+++ app/src/main/java/com/taller2/hypechatapp/ui/activities/ChatActivity.java
@@ -0,0 +1,14 @@
+package com.taller2.hypechatapp.ui.activities;
+
+import android.os.Bundle;
+
+import com.taller2.hypechatapp.R;
+
+public class ChatActivity extends MenuActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_chat);
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/taller2/hypechatapp/ui/activities/LoginActivity.java
+++ app/src/main/java/com/taller2/hypechatapp/ui/activities/LoginActivity.java
@@ -154,7 +154,7 @@ public class LoginActivity extends AppCompatActivity {
 
     public void userLoggedIn(){
         Log.i("LoginActivity","User is logged in with token: " + FirebaseAuthService.getCurrentUserToken());
-        Intent intent = new Intent(this, ChannelChatActivity.class);
+        Intent intent = new Intent(this, ChatActivity.class);
         startActivity(intent);
         finish();
     }
