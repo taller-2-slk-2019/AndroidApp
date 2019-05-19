8ca71612544ca83bb7a406d6fdf7dc48e7d3a42d
matias feld <matias.feld@mdevz.com>
2019-05-19 16:02:09 -0300

    confirm dialog for channel abandon

---
+++ app/src/main/java/com/taller2/hypechatapp/components/DialogService.java
@@ -0,0 +1,30 @@
+package com.taller2.hypechatapp.components;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.Log;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class DialogService {
+
+    public static void showConfirmDialog(Context context, String message, final DialogConfirm listener) {
+        new AlertDialog.Builder(context)
+                .setTitle("Atención")
+                .setMessage(message)
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("ConfirmDialog", "Cancel");
+                    }
+                })
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("ConfirmDialog", "Confirm");
+                        listener.onConfirm();
+                    }
+                })
+                .show();
+    }
+}
