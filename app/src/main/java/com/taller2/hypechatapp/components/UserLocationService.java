720ae9bbf89b9abe347b62c7bd55968f7cd6d694
matias feld <matias.feld@mdevz.com>
2019-05-25 15:03:59 -0300

    fixed location

--- app/src/main/java/com/taller2/hypechatapp/components/UserLocationService.java
+++ app/src/main/java/com/taller2/hypechatapp/components/UserLocationService.java
@@ -6,29 +6,39 @@ import android.location.Location;
 import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 
 public class UserLocationService {
+
+    private static String TAG = "User location";
 
     @SuppressLint("MissingPermission") // permission must be requested
     public static void getUserLocation(Activity activity, final UserLocationListener listener) {
         FusedLocationProviderClient locationClient = LocationServices.getFusedLocationProviderClient(activity);
 
-        locationClient.getLastLocation()
-                .addOnSuccessListener(new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-                            // Logic to handle location object
-                            Log.d("User location", "Latitude: " + location.getLatitude());
-                            Log.d("User location", "Longitude: " + location.getLongitude());
-                            listener.userLocationReceived(location);
-                        } else {
-                            listener.userLocationError();
-                        }
-                    }
-                });
+        LocationRequest request = LocationRequest.create();
+        request.setNumUpdates(1);
+        request.setExpirationDuration(2000);
+
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    Log.d(TAG, "No location");
+                    listener.userLocationError();
+                } else {
+                    Location location = locationResult.getLastLocation();
+                    Log.d(TAG, "Latitude: " + location.getLatitude());
+                    Log.d(TAG, "Longitude: " + location.getLongitude());
+                    listener.userLocationReceived(location);
+                }
+            }
+        };
+
+        locationClient.requestLocationUpdates(request, locationCallback, null);
     }
 
     public interface UserLocationListener {
